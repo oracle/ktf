oracle/ktf

commit 2db0716e3ccfc5ec5fc850061d18fdac63509871

support kthread creation in KTF
some tests will require multiple threads.  We want to support
assertions in those thread too, so we need to wrap kthread_*()
interfaces to pass in test context.  We also provide a means to
wait for thread completion.  Interfaces are

KTF_THREAD_INIT(name, t);
KTF_THREAD_RUN(t);
KTF_THREAD_STOP(t);
KTF_THREAD_WAIT_STARTED(t);
KTF_THREAD_WAIT_COMPLETED(t);

t is a struct ktf_thread *.  Threads are defined as:

KTF_THREAD(name)
{
        ...
}

Change-Id: I8946037282c291d7ed8eb351c65df9e393e15b6c
Signed-off-by: Alan Maguire <alan.maguire@oracle.com>

kernel/kcheck.c
@@ -160,6 +160,13 @@ void flush_assert_cnt(struct ktf_test *self)
 	}
 }
 
+u32 ktf_get_assertion_count(void)
+{
+	return assert_cnt;
+}
+EXPORT_SYMBOL(ktf_get_assertion_count);
+
+DEFINE_SPINLOCK(assert_lock);
 
 long _fail_unless (struct ktf_test *self, int result, const char *file,
 			int line, const char *fmt, ...)
@@ -168,6 +175,10 @@ long _fail_unless (struct ktf_test *self, int result, const char *file,
 	va_list ap;
 	char *buf;
 	char bufprefix[256];
+	unsigned long flags;
+
+	/* Multiple threads may try to update log/count */
+	spin_lock_irqsave(&assert_lock, flags);
 
 	if (result)
 		assert_cnt++;
@@ -175,7 +186,7 @@ long _fail_unless (struct ktf_test *self, int result, const char *file,
 		flush_assert_cnt(self);
 		buf = (char*)kmalloc(MAX_PRINTF, GFP_KERNEL);
 		if (!buf)
-			return result;
+			goto out;
 		va_start(ap,fmt);
 		len = vsnprintf(buf,MAX_PRINTF-1,fmt,ap);
 		buf[len] = 0;
@@ -194,6 +205,8 @@ long _fail_unless (struct ktf_test *self, int result, const char *file,
 		(void) strncat(self->log, buf, KTF_MAX_LOG);
 		kfree(buf);
 	}
+out:
+	spin_unlock_irqrestore(&assert_lock, flags);
 	return result;
 }
 EXPORT_SYMBOL(_fail_unless);

kernel/kcheck.h
@@ -89,6 +89,17 @@ struct ktf_case {
 	struct ktf_debugfs debugfs; /* debugfs handles for testset */
 };
 
+/* Used for tests that spawn kthreads to pass state.  We should probably
+ * look at passing data to tests like this to make things more extensible,
+ * but will defer for now as this would disrupt KTF consumers.
+ */
+struct ktf_test_state {
+	struct ktf_test *self;
+	struct ktf_context *ctx;
+	int iter;
+	u32 value;
+};
+
 void ktf_debugfs_create_test(struct ktf_test *);
 void ktf_debugfs_destroy_test(struct ktf_test *);
 void ktf_debugfs_create_testset(struct ktf_case *);

kernel/ktf.h
@@ -13,6 +13,7 @@
 
 #include <linux/completion.h>
 #include <linux/kprobes.h>
+#include <linux/kthread.h>
 #include "kcheck.h"
 #include "ktf_map.h"
 #include "unlproto.h"
@@ -26,6 +27,18 @@ struct ktf_context {
 
 /* type for a test function */
 struct ktf_test;
+
+/* state of running test, used to pass to threads spawned by test. */
+struct ktf_test_state;
+
+struct ktf_thread {
+	int (*func)(void *);
+	const char *name;
+	struct task_struct *task;
+	struct ktf_test_state state;
+	struct completion started;
+	struct completion completed;
+};
 
 typedef void (*ktf_test_adder)(void);
 
@@ -135,6 +148,51 @@ extern struct ktf_handle __test_handle;
 
 #define KTF_UNREGISTER_RETURN_PROBE(func, probehandler) \
 	KTF_UNREGISTER_PROBE(handler, func, probehandler)
+
+/* Interfaces for creating kthreads in tests. */
+#define	KTF_THREAD_INIT(threadname, t) \
+	do { \
+		(t)->func = threadname; \
+		(t)->name = #threadname; \
+		(t)->state.self = self; \
+		(t)->state.ctx = ctx; \
+		(t)->state.iter = _i; \
+		(t)->state.value = _value; \
+		init_completion(&((t)->started)); \
+		init_completion(&((t)->completed)); \
+	} while (0)
+
+#define	KTF_THREAD_RUN(t) \
+	((t)->task = kthread_run((t)->func, t, (t)->name))
+
+#define KTF_THREAD_STOP(t) \
+	do { \
+		if ((t)->task) \
+			kthread_stop((t)->task); \
+	} while (0)
+
+/* Wraps thread execution to supply same variables as test case - this allows
+ * us to define assertions etc in thread context.
+ */
+#define	KTF_THREAD(name) \
+	static void __##name(struct ktf_thread *thread, struct ktf_test *self, \
+			     struct ktf_context *ctx, int _i, u32 _value); \
+	int name(void *data) \
+	{ \
+		struct ktf_thread *t = data; \
+		complete(&t->started); \
+		__##name(t, t->state.self, t->state.ctx, t->state.iter, \
+			 t->state.value); \
+		complete(&t->completed); \
+		return 0; \
+	} \
+	static void __##name(struct ktf_thread *_thread, struct ktf_test *self,\
+			     struct ktf_context *ctx, int _i, u32 _value)
+
+#define	KTF_THREAD_WAIT_STARTED(t)	(wait_for_completion(&((t)->started)))
+#define	KTF_THREAD_WAIT_COMPLETED(t)	(wait_for_completion(&((t)->completed)))
+
+u32 ktf_get_assertion_count(void);
 
 /**
  * ASSERT_TRUE() - fail and return if @C evaluates to false

selftest/self.c
@@ -339,6 +339,38 @@ static void add_cov_tests(void)
 	ADD_TEST(cov);
 }
 
+KTF_THREAD(test_thread)
+{
+	/* ensure assertions can work in thread context */
+	ASSERT_INT_EQ(1, 1);
+}
+
+#define NUM_TEST_THREADS 20
+
+struct ktf_thread test_threads[NUM_TEST_THREADS];
+
+TEST(selftest, thread)
+{
+	int assertions, i;
+
+	for (i = 0; i < NUM_TEST_THREADS; i++) {
+		KTF_THREAD_INIT(test_thread, &test_threads[i]);
+		KTF_THREAD_RUN(&test_threads[i]);
+	}
+	for (i = 0; i < NUM_TEST_THREADS; i++)
+		KTF_THREAD_WAIT_COMPLETED(&test_threads[i]);
+
+	assertions = (int)ktf_get_assertion_count();
+
+	/* Verify assertion in thread */
+	ASSERT_INT_EQ(assertions, NUM_TEST_THREADS);
+}
+
+static void add_thread_tests(void)
+{
+	ADD_TEST(thread);
+}
+
 static int __init selftest_init(void)
 {
 	int ret = ktf_context_add(&dual_handle, &s_mctx[0].k, "map1");
@@ -359,6 +391,7 @@ static int __init selftest_init(void)
 	add_map_tests();
 	add_probe_tests();
 	add_cov_tests();
+	add_thread_tests();
 	tlog(T_INFO, "selftest: loaded\n");
 	return 0;
 }

commit 4aac0b43c8999fd9b0d5c66ce554e9a8c7182511

Fix potential sleep while atomic and avoid lock in the common case
The introduction of an irq safe spin lock to protect
assertion count and log state in commit 2db0716e3c
caused the kmalloc to be called with interrupts off
which creates a potential deadlock scenario.

The case where tests pass is the performance critical
part and we also want to minimize any effects by KTF on
ordering. Optimize for the common case by only using the lock
in the exception path, and only to protect the log.

For the assertion count we instead use an atomic
which is safe and does not affect interrupt handling.
Alternatively we could just leave it unsafe as many tests are sequential
anyway and for those that are not, it does not have to be exact,
but that's likely to create ambiguous rare confusing situations
in threaded tests so lets keep it synchronized for now.

Change-Id: Ie6518ba13c44d5364931cb98940688ebf1e7b4c5
Signed-off-by: Knut Omang <knut.omang@oracle.com>

kernel/kcheck.c
@@ -148,21 +148,21 @@ struct ktf_case *ktf_case_find_create(const char *name)
 	return tc;
 }
 
-u32 assert_cnt = 0;
+atomic_t assert_cnt = ATOMIC_INIT(0);
 
 void flush_assert_cnt(struct ktf_test *self)
 {
-	if (assert_cnt) {
-		tlog(T_DEBUG, "update: %d asserts", assert_cnt);
+	if (atomic_read(&assert_cnt)) {
+		tlog(T_DEBUG, "update: %d asserts", atomic_read(&assert_cnt));
 		if (self->skb)
-			nla_put_u32(self->skb, KTF_A_STAT, assert_cnt);
-		assert_cnt = 0;
+			nla_put_u32(self->skb, KTF_A_STAT, atomic_read(&assert_cnt));
+		atomic_set(&assert_cnt, 0);
 	}
 }
 
 u32 ktf_get_assertion_count(void)
 {
-	return assert_cnt;
+	return atomic_read(&assert_cnt);
 }
 EXPORT_SYMBOL(ktf_get_assertion_count);
 
@@ -177,16 +177,16 @@ long _fail_unless (struct ktf_test *self, int result, const char *file,
 	char bufprefix[256];
 	unsigned long flags;
 
-	/* Multiple threads may try to update log/count */
-	spin_lock_irqsave(&assert_lock, flags);
-
 	if (result)
-		assert_cnt++;
+		atomic_inc(&assert_cnt);
 	else {
 		flush_assert_cnt(self);
 		buf = (char*)kmalloc(MAX_PRINTF, GFP_KERNEL);
-		if (!buf)
+		if (!buf) {
+			tlog(T_ERROR,"file %s line %d: Unable to allocate memory for the error report!",
+				file, line);
 			goto out;
+		}
 		va_start(ap,fmt);
 		len = vsnprintf(buf,MAX_PRINTF-1,fmt,ap);
 		buf[len] = 0;
@@ -201,12 +201,15 @@ long _fail_unless (struct ktf_test *self, int result, const char *file,
 				"file %s line %d: result %d: ", file, line,
 				result);
 		tlog(T_ERROR, "%s%s", bufprefix, buf);
+
+		/* Multiple threads may try to update log */
+		spin_lock_irqsave(&assert_lock, flags);
 		(void) strncat(self->log, bufprefix, KTF_MAX_LOG);
 		(void) strncat(self->log, buf, KTF_MAX_LOG);
+		spin_unlock_irqrestore(&assert_lock, flags);
 		kfree(buf);
 	}
 out:
-	spin_unlock_irqrestore(&assert_lock, flags);
 	return result;
 }
 EXPORT_SYMBOL(_fail_unless);